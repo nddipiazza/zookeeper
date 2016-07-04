c6f17b36d94e442cf4589ee3cf9e021eb65ed425
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2016-07-04 10:25:02 -0500

    Update zkFileUpload.sh

--- bin/zkFileUpload.sh
+++ bin/zkFileUpload.sh
@@ -4,12 +4,11 @@ print_usage () {
     echo "Usage: $0 {server:port} set {path} {fileName}"
     echo "  Upload {data} from {fileName} and write into {path}"   
 }
- 
 if [ "$#" -ne 4 ]; then
     print_usage 
 elif [ "$2" = "set" ]; then
     fileContent=$(cat $4)
-    bin/zkCli.sh -server $1 $2 $3 "$fileContent"
+    `dirname $0`/zkCli.sh -server $1 $2 $3 "$fileContent"
 else
     print_usage
 fi
